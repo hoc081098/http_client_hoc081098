e8eaeb2d33e36b71ca35e9e808147878ebc1966c
petrus hoc <hoc081098@gmail.com>
2022-08-31 22:56:45 +0700

    Initial commit - with love

---
+++ example/http_client_hoc081098_example.dart
@@ -0,0 +1,6 @@
+import 'package:http_client_hoc081098/http_client_hoc081098.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/http_client_hoc081098.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library http_client_hoc081098;
+
+export 'src/http_client_hoc081098_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
