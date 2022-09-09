34827233642ee1164486ad5e8dbd1f7970098666
petrus hoc <hoc081098@gmail.com>
2022-09-09 09:58:15 +0700

    tests(get)

--- test/tests/get_test.dart
+++ test/tests/get_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:http_client_hoc081098/http_client_hoc081098.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
@@ -30,7 +32,7 @@ void main() {
       requests.clear();
     });
 
-    test('success', () async {
+    test('200 response', () async {
       when(mockClient.send(any)).thenAnswer(
         (_) => responseToStreamedResponse(
           http.Response(
@@ -50,7 +52,7 @@ void main() {
       expect(requests[0].url, getUri('users/1'));
     });
 
-    test('failure', () async {
+    test('non-200 response', () async {
       when(mockClient.send(any)).thenAnswer(
         (_) => responseToStreamedResponse(
           http.Response(
@@ -65,6 +67,21 @@ void main() {
 
       expect(response.statusCode, 500);
       expect(response.body, getFixtureString('error.json'));
+
+      expect(requests.length, 1);
+      expect(requests[0].url, getUri('users/1'));
+    });
+
+    test('throw exception', () async {
+      when(mockClient.send(any)).thenAnswer(
+        (_) => throw const SocketException.closed(),
+      );
+
+      await expectLater(
+        simpleClient.get(getUri('users/1')),
+        throwsA(isA<SocketException>()),
+      );
+      verify(mockClient.send(any)).called(1);
 
       expect(requests.length, 1);
       expect(requests[0].url, getUri('users/1'));
