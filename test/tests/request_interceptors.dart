2d672ad8350ef8b124f60f6378b7a6eb8da314e2
petrus hoc <hoc081098@gmail.com>
2022-12-11 00:18:10 +0700

    test(requestInterceptors): cancel request if cancel token is cancelled when running the last interceptor

--- test/tests/request_interceptors.dart
+++ test/tests/request_interceptors.dart
@@ -86,5 +86,29 @@ void main() {
         expect(requestsSpy2.requests.isEmpty, isTrue);
       },
     );
+
+    test(
+      'cancel request if cancel token is cancelled when running the last interceptor',
+      () async {
+        final token = CancellationToken();
+        requestsSpy2.addOnCallListener((_) => token.cancel());
+
+        final responseFuture = simpleClient.get(
+          getUri('users/1'),
+          headers: mockHeaders,
+          cancelToken: token,
+        );
+
+        await expectLater(responseFuture, throwsACancellationException);
+
+        // Two interceptors are called with same request.
+        expect(requestsSpy1, requestsSpy2);
+
+        // The first interceptor is called.
+        expect(requestsSpy1.requests.single.url, getUri('users/1'));
+        expect(requestsSpy1.requests.single.method, 'GET');
+        expectMockHeaders(requestsSpy1.requests.single);
+      },
+    );
   });
 }
